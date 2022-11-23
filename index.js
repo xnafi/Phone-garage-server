ddd91e16969478d5526a51161fa557e33096862f
Forhad Khan <fk.nafi11@gmail.com>
2022-11-23 23:29:47 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+const cors = require('cors');
+const  = require('jwt');
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
