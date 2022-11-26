cc391dd7a58fb3b73114200df2f7df323ffed636
Forhad Khan <fk.nafi11@gmail.com>
2022-11-27 05:30:32 +0600

    item collection added and data posted

--- index.js
+++ index.js
@@ -26,71 +26,72 @@ const run = async () => {
         // Posted Items
         const postCollection = client.db('phone_garage').collection('postItems')
 
-        // brand catagory
+
         app.get('/brands', async (req, res) => {
             const query = {}
             const result = await brandCollection.find(query).toArray()
             res.send(result)
         })
-        // get catagory by id
         app.get('/brand/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) }
             const result = await brandCollection.findOne(query)
             res.send(result)
         })
-        // get add a user
         app.post('/users', async (req, res) => {
             const user = req.body
-
             const result = await usersCollection.insertOne(user)
             res.send(result)
         })
-        // get users
         app.get('/users', async (req, res) => {
             const query = {}
             const result = await usersCollection.find(query).toArray()
             res.send(result)
         })
-        // get user by email
-        app.get('/users/:email', async (req, res) => {
-            const email = req.params.email
-            const query = { email: email }
-            const result = await usersCollection.findOne(query)
-            res.send(result)
-        })
-        // filter out sellers
         app.get('/users/sellers', async (req, res) => {
             const query = { role: "seller" }
             const result = await usersCollection.find(query).toArray()
             res.send(result)
         })
-        // delete a seller
         app.delete('/users/sellers/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) }
             const result = await usersCollection.deleteOne(query)
             res.send(result)
         })
-        // filter out buyers
         app.get('/users/buyers', async (req, res) => {
             const query = { role: "buyer" }
             const result = await usersCollection.find(query).toArray()
             res.send(result)
         })
-        // delete a buyer
         app.delete('/users/buyers/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) }
             const result = await usersCollection.deleteOne(query)
             res.send(result)
         })
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { email: email }
+            const result = await usersCollection.findOne(query)
+            res.send(result)
+        })
+        // add a item
         app.post('/items', async (req, res) => {
             const item = req.body
             const result = await postCollection.insertOne(item)
             res.send(result)
 
         })
+        // get all items
+        app.get('/items', async (req, res) => {
+            const query = req.query.name
+            const filter = { name: query }
+            const result = await postCollection.find(filter).toArray()
+            res.send(result)
+        })
+
+
     } catch (error) {
         console.log(error)
     }
